4329ebc5597554f0fbd22c0e3f56d7e249a122ac
krishna <kk293781@gmail.com>
2020-05-28 23:22:38 +0530

    created git ignore file

--- app.js
+++ app.js
@@ -140,6 +140,12 @@ app.post("/delete",function(req,res){
     
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
 app.listen(3000,function(){
     console.log("Server is runing")
 })
